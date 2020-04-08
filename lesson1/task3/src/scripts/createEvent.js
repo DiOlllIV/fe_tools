e076cdda4fd0146ee597c818383c12f9aa8971d0
Denis Olekh <denisolekh@gmail.com>
2020-04-08 20:04:58 +0300

    hw

---
+++ lesson1/task3/src/scripts/createEvent.js
@@ -0,0 +1,40 @@
+import { renderEventItem } from './renderEvents.js';
+import { setItem } from './storage.js';
+import { createEvents, getEventsList } from './eventsGateway.js';
+
+export const createEvent = () => {
+
+    let eventId = 0;
+    const eventTitle = document.querySelector('.pop-up__title');
+    const date = document.querySelector('.time-set__date');
+    const startTime = document.querySelector('.time-set__start');
+    const endTime = document.querySelector('.time-set__end');
+    const eventComment = document.querySelector('.pop-up__comment');
+    const introducedDateValue = date.value && startTime.value && endTime.value;
+    if (!introducedDateValue)
+        return;
+
+    const newEvent = {
+        id: eventId,
+        title: eventTitle.value,
+        startDate: `${date.value} ${startTime.value}`,
+        endDate: `${date.value} ${endTime.value}`,
+        comment: eventComment.value
+    };
+
+    eventTitle.value = '';
+    date.value = '';
+    startTime.value = '';
+    endTime.value = '';
+    eventComment.value = '';
+
+    createEvents(newEvent)
+        .then(getEventsList)
+        .then(newEventsList => {
+            setItem('eventsList', newEventsList);
+            renderEventItem();
+        });
+
+
+    document.getElementById('popup').style.visibility = "hidden";
+};
---
+++ lesson1/task3/src/scripts/delete.js
@@ -0,0 +1,72 @@
+import { renderEventItem } from './renderEvents.js';
+import { deleteEvent, getEventsList } from './eventsGateway.js';
+import { setItem } from './storage.js';
+
+export function deleteBtn() {
+    const eventsElem = document.querySelectorAll('.event');
+    let click = false;
+
+    /* for (let i = 0; i < eventsElem.length; i++) {
+        eventsElem[i].addEventListener('click', function() {
+
+            if (!click) {
+
+                let eventElem = eventsElem[i].id;
+                const visibleBtn = document.getElementById(`delete${eventElem}`);
+
+
+                click = true;
+                visibleBtn.style.visibility = 'visible';
+                visibleBtn.addEventListener('click', deleteEventElem);
+
+            } else if (click) {
+                click = false;
+                let eventElem = eventsElem[i].id;
+                document.getElementById(`delete${eventElem}`).style.visibility = 'hidden';
+            }
+        });
+
+        function deleteEventElem() {
+            if (!deleteBtn)
+                return;
+
+            let eventElem = eventsElem[i].id;
+            return deleteEvent(eventElem)
+                .then(() => getEventsList())
+                .then(newEventsList => {
+                    setItem('eventsList', newEventsList);
+                    renderEventItem();
+                });
+        };
+    } */
+    /************************************************************************************************/
+
+    eventsElem.forEach(elem => {
+
+        const deleteEventElem = () =>
+            deleteEvent(elem.id)
+            .then(() => getEventsList())
+            .then(newEventsList => {
+                setItem('eventsList', newEventsList);
+                renderEventItem();
+            });
+
+
+        function activateBtn() {
+
+            if (!click) {
+                const visibleBtn = document.getElementById(`delete${elem.id}`);
+
+                click = true;
+                visibleBtn.style.visibility = 'visible';
+                visibleBtn.addEventListener('click', deleteEventElem);
+
+            } else if (click) {
+                click = false;
+                document.getElementById(`delete${elem.id}`).style.visibility = 'hidden';
+            }
+        };
+
+        elem.addEventListener('click', activateBtn);
+    });
+};
