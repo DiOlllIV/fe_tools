88f082a02571edd325fa5b8fe8b9cdf7842a2be6
Denis Olekh <denisolekh@gmail.com>
2020-04-11 01:15:50 +0300

    hw

--- lesson6/task4/webpack.config.js
+++ lesson6/task4/webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
                 use: [{
                     loader: 'url-loader',
                     options: {
-                        limit: 8192,
+                        limit: 8,
                         name: '[name].[ext]',
                         outputPath: 'images',
                     },
